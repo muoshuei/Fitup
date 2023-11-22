73fd422b2a776ff351a8909e20cb7d00afc79771
muoshuei <llh03098@gmail.com>
2023-11-22 18:11:42 +0800

    Feature: more reliable action detections

--- frontend/src/components/Train/modules/ExerciseCountManager.js
+++ frontend/src/components/Train/modules/ExerciseCountManager.js
@@ -20,16 +20,31 @@ class ExerciseCountManager{
         this.counterPartsState = [true, true];
     }
     check(angles){
-        let Langle = angles[0];
-        let Rangle = angles[1];
-        let LState = this.counterPartsState[0];
-        let RState = this.counterPartsState[1];
-        if (LState && RState && Langle < this.counterLB && Rangle < this.counterLB) {
-            this.counterPartsState = [false, false];
-        } else if (!LState && !RState && Langle > this.counterUB && Rangle > this.counterUB) {
-            this.counterPartsState = [true, true];
-            this.count++;
+        console.log([this.counterLB, this.counterUB]);
+
+        if(angles.length === 1){
+            let angle = angles[0];
+            let LState = this.counterPartsState[0];
+            if (LState && angle < this.counterLB) {
+                this.counterPartsState = [false, true];
+            } else if (!LState && angle > this.counterUB) {
+                this.counterPartsState = [true, true];
+                this.count++;
+            }
         }
+        else{
+            let Langle = angles[0];
+            let Rangle = angles[1];
+            let LState = this.counterPartsState[0];
+            let RState = this.counterPartsState[1];
+            if (LState && RState && Langle < this.counterLB && Rangle < this.counterLB) {
+                this.counterPartsState = [false, false];
+            } else if (!LState && !RState && Langle > this.counterUB && Rangle > this.counterUB) {
+                this.counterPartsState = [true, true];
+                this.count++;
+            }
+        }
+        
     }
     
 }
